0d94504be4a4f779cdd5f5141d5c09f98ab19006
itisem <emily@itisem.com>
2023-07-03 16:24:15 +0200

    base64 string from url

--- parsers/base64-string.ts
+++ parsers/base64-string.ts
@@ -15,4 +15,15 @@ export default class Base64StringPBFField extends SimplePBFField<string>{
 		// making it url safe and removing padding since base
 		return base64.replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "");
 	}
+
+	// does NOT handle full urls, just the decoding (for strings)
+	protected decodeValue(value?: string | undefined): string | undefined{
+		if(value === undefined) return undefined;
+		const paddingCount = (-value.length + 4 * value.length) % 4;
+		const padded = value.replaceAll("-", "+").replaceAll("_", "/") + "=".repeat(paddingCount);
+		// another unicode messing around
+		const asciiValue = atob(padded);
+		const bytes = Uint8Array.from(asciiValue, x => x.codePointAt(0));
+		return new TextDecoder().decode(bytes);
+	}
 }
