700175a148ea051c85daf22b72ae8770f70a91cf
itisem <emily@itisem.com>
2023-11-27 15:10:28 +0100

    redoing how definition expansion works to reduce recursive calls

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 // re-exporting everything to have them accessible for anyone who needs the raw parsers for any reason
 import MessagePBFField from "./parsers/message";
 
+import _ from "lodash";
+
 import type {DescriptionObj, CoreDefinition} from "./parsers/message";
 
 // the outer layer will always have nested, so exporting IndividualProtobufDefinition and nothing else would be incorrect
@@ -39,15 +41,22 @@ interface FieldDefinition{
 export default class ProtobufParser{
 	protected definition: ManyProtobufDefinitions;
 	protected loadedDefinitions: {[key: string]: DescriptionObj};
+	protected underConstructionDefinition: DescriptionObj;
 
 	constructor(definition: ProtobufDefinition | string){
 		// assumes that all jsons are correct. if they are not, this will cause a ruckus later
 		if(typeof definition === "string") this.definition = (JSON.parse(definition) as ProtobufDefinition).nested;
 		else this.definition = definition.nested;
 		this.loadedDefinitions = {};
+		this.underConstructionDefinition = {};
 	}
 
 	create(field: string): MessagePBFField{
+		// list of static field types that don't require expanding
+		const staticFields = [
+			"bool", "bytes", "double", "fixed32", "fixed64", "float", "int32", "int64",
+			"sfixed32", "sfixed64", "sint32", "sint64", "string", "uint32", "uint64"
+		];
 		// a saved definition already exists, no need to reconstruct it from scratch
 		if(this.loadedDefinitions[field]){
 			const m = new MessagePBFField({}, {});
@@ -59,50 +68,92 @@ export default class ProtobufParser{
 			const fieldDefinition = this.definition[field];
 			if(!fieldDefinition) throw new Error("Non-existent field referenced");
 			if(!fieldDefinition.fields) throw new Error("Only message fields can be created");
-			let finalField: DescriptionObj = {};
-			const nested = fieldDefinition.nested;
-			for(let fieldName in fieldDefinition.fields){
-				finalField[fieldName] = this.expandCreate(fieldDefinition.fields[fieldName], nested);
+			// should always be empty, but clearing just in case something incomprehensibly weird happens
+			let newDefintion: DescriptionObj = {};
+			const baseNested = fieldDefinition.nested;
+			// for large definitions, using an elegant(ish) recursive solution makes the call stack exceed
+			// so instead, we expand layer by layer in the same function (even though it's a lot less nice)
+			let expansions: string[] = [];
+			let expansionDefinitions: IndividualProtobufDefinition[] = [];
+			// initialise the expansion first
+			for(let key in fieldDefinition.fields){
+				// get a list of nested definitions for type lookup
+				const nestedKeys = baseNested ? Object.keys(baseNested) : [];
+				const field = fieldDefinition.fields[key];
+				if(staticFields.includes(field.type)){
+					// for any static field, we are safe to just use the definition itself
+					newDefintion[key] = field as CoreDefinition;
+				}
+				else{
+					expansions.push(key); // the definition needs expanding
+					// include the correct definition
+					if(nestedKeys.includes(field.type)){
+						expansionDefinitions.push(baseNested[field.type]);
+					}
+					else{
+						if(!this.definition[field.type]) throw new Error("Non-existent field type " + field.type);
+						expansionDefinitions.push(this.definition[field.type]);
+					}
+					newDefintion[key] = {
+						type: "??",
+						id: field.id,
+						rule: field.rule
+					}
+				}
+			}
+			// we do much of the same as we did for initialising, just through a little bit of extra nonsense
+			while(expansions.length > 0){
+				let newExpansions: string[] = [];
+				let newExpansionDefinitions: IndividualProtobufDefinition[] = [];
+				for(let i = 0; i < expansions.length; i++){
+					const expansion = expansions[i];
+					const definition = expansionDefinitions[i];
+					// handle enums first
+					if(definition.values){
+						_.set(newDefintion, expansion + ".type", "enum");
+						_.set(newDefintion, expansion + ".values", definition.values);
+					}
+					else{
+						// this must be a message type now
+						_.set(newDefintion, expansion + ".type", "message");
+						const fieldDescription: DescriptionObj = {};
+						const nested = definition.nested;
+						const nestedKeys = nested ? Object.keys(nested) : [];
+						// since this is a message, fields must exist
+						// what happens here is practically identical to what happened for the initial expansion
+						for(let key in definition.fields){
+							let field = definition.fields[key];
+							if(staticFields.includes(field.type)){
+								fieldDescription[key] = field as CoreDefinition;
+							}
+							else{
+								newExpansions.push(expansion + ".fields." + key);
+								if(nestedKeys.includes(field.type)){
+									newExpansionDefinitions.push(nested[field.type]);
+								}
+								else{
+									if(!this.definition[field.type]) throw new Error("Non-existent field type " + field.type);
+									newExpansionDefinitions.push(this.definition[field.type]);
+								}
+								fieldDescription[key] = {
+									type: "??",
+									id: field.id,
+									rule: field.rule
+								};
+							}
+						}
+						_.set(newDefintion, expansion + ".fields", fieldDescription);
+					}
+				}
+				expansions = newExpansions;
+				expansionDefinitions = newExpansionDefinitions;
 			}
 			// save definition for later use
 			// don't save intermediate definitions to conserve memory
-			this.loadedDefinitions[field] = finalField;
+			this.loadedDefinitions[field] = newDefintion;
 			const m = new MessagePBFField({}, {});
-			m.from(finalField);
+			m.from(newDefintion);
 			return m;
-		}
-	}
-
-	protected expandCreate(field: FieldDefinition, nested?: ManyProtobufDefinitions): CoreDefinition{
-		const staticFields = [
-			"bool", "bytes", "double", "fixed32", "fixed64", "float", "int32", "int64",
-			"sfixed32", "sfixed64", "sint32", "sint64", "string", "uint32", "uint64"
-		];
-		const nestedKeys = nested ? Object.keys(nested) : [];
-		if(staticFields.includes(field.type)) return field as CoreDefinition; // the FieldDefinition and CoreDefinition format are equivalent in this case
-		// we have an enum or a message
-		let foundDefinition: IndividualProtobufDefinition;
-		// find the field in our nesting, and if not, look at the top level
-		if(nestedKeys.includes(field.type)){
-			foundDefinition = nested[field.type];
-		}
-		else{
-			if(!this.definition[field.type]) throw new Error("Non-existent field type " + field.type);
-			foundDefinition = this.definition[field.type];
-		}
-
-		// enum definitions will always have a values field
-		if(foundDefinition.values){
-			return {...field, type: "enum", values: foundDefinition.values};
-		}
-		// if not, it's a message definition
-		else{
-			let finalField: DescriptionObj = {};
-			const nested = foundDefinition.nested;
-			for(let fieldName in foundDefinition.fields){
-				finalField[fieldName] = this.expandCreate(foundDefinition.fields[fieldName], nested);
-			}
-			return {...field, type: "message", fields: finalField};
 		}
 	}
 }
