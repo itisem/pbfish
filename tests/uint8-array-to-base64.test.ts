5d0f8ef284671d79b1d8afa7542a1c0c0530102a
itisem <emily@itisem.com>
2024-01-29 15:54:21 +0100

    testing uint8array conversion

--- tests/uint8-array-to-base64.test.ts
+++ tests/uint8-array-to-base64.test.ts
@@ -2,6 +2,8 @@ import uint8ArrayToBase64 from "../util/uint8array-to-base64";
 
 describe("uint8ArrayToBase64", () => {
 	test("converts correctly", () => {
-		expect(uint8ArrayToBase64(new Uint8Array([72, 101, 108, 108, 111]))).toEqual("SGVsbG8=");
+		expect(
+			uint8ArrayToBase64(new Uint8Array([72, 101, 108, 108, 111]))
+		).toEqual("SGVsbG8=");
 	});
 })
